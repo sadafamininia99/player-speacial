ab28a9bc3f505dbbf2c0c2c9e3d7e102693630e2
= <aminicomputer96@gmail.com>
2021-05-16 16:23:28 +0430

    we create some speacial video player with css and js and also html contoles re degsined by css

---
+++ script.js
@@ -0,0 +1,5 @@
+const video = document.getElementById('video');
+const paly = document.getElementById('paly');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
