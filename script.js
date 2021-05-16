a6f59cb8e9fbe0b7debb10d7865a5e8cf6e088c1
= <aminicomputer96@gmail.com>
2021-05-16 21:16:08 +0430

    i add some function for control

--- script.js
+++ script.js
@@ -1,5 +1,52 @@
-const video = document.getElementById('video');
-const paly = document.getElementById('paly');
-const stop = document.getElementById('stop');
-const progress = document.getElementById('progress');
-const timestamp = document.getElementById('timestamp');
+    const video = document.getElementById('video');
+    const paly = document.getElementById('paly');
+    const stop = document.getElementById('stop');
+    const progress = document.getElementById('progress');
+    const timestamp = document.getElementById('timestamp');
+
+    //play and pause video
+    function toggleVideoStatus(){
+    if(video.paused){
+        video.paly();
+    }else{
+        video.pause();
+    }
+    }
+
+    //update paly pause icon
+    function updatePlayIcon(){
+        if (video.paused)
+        {
+            paly.innerHTML='<i class=fa fa-play fa-2x></i>';
+
+        }else{
+            paly.innerHTML='<i class=fa fa-pause fa-2x></i>';
+        }
+    }
+    //update progress and timestamp
+    function updateprogress(){
+        return true;
+    }
+    //set video tikme to progress
+    function setVideoProgress(){
+        return true;
+    }
+    //stop video
+    function stopVideo(){
+        return true;
+    }
+
+
+    //Event listiner
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
