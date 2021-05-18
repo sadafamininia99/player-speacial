69a9d8bf3b969c181c1872034e71351d1b907feb
= <aminicomputer96@gmail.com>
2021-05-18 11:10:21 +0430

    add js

--- script.js
+++ script.js
@@ -1,43 +1,56 @@
-    const video = document.getElementById('video');
-    const paly = document.getElementById('paly');
-    const stop = document.getElementById('stop');
-    const progress = document.getElementById('progress');
-    const timestamp = document.getElementById('timestamp');
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
 
-    //play and pause video
-    function toggleVideoStatus(){
-    if(video.paused){
-        video.paly();
-    }else{
-        video.pause();
-    }
-    }
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
-    //update paly pause icon
-    function updatePlayIcon(){
-        if (video.paused)
-        {
-            paly.innerHTML='<i class=fa fa-play fa-2x></i>';
+// update play/pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
 
-        }else{
-            paly.innerHTML='<i class=fa fa-pause fa-2x></i>';
-        }
-    }
-    //update progress and timestamp
-    function updateprogress(){
-        return true;
-    }
-    //set video tikme to progress
-    function setVideoProgress(){
-        return true;
-    }
-    //stop video
-    function stopVideo(){
-        return true;
-    }
+// Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
 
+  // Get the minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if(mins < video.duration){
+    mins = '0' + String(mins);
+  }
 
-    //Event listiner
+  // Get Seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if(secs < video.duration){
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
 
 // Event listeners
 video.addEventListener('click', toggleVideoStatus);
